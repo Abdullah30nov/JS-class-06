0a72367725faeff5ffbc151b794e303c6b600473
Abdullah30nov <sheheryar.hill.277@gmail.com>
2024-03-16 12:00:57 +0500

    JS-class-06 code for Loop

---
+++ app.js
@@ -0,0 +1,35 @@
+// var userinput = prompt("Enter Your City Name");
+// var capital = userinput . toUpperCase();
+// document.write(capital);
+
+
+// var text = "My name is Abdullah! bin aslam"
+
+// for(var i=0;i < text.length;i++){
+//     if (text.slice(i,i+1) === "!"){
+//         alert("Found !");
+//     }
+// }
+
+
+
+// var userinput = prompt("Enter your city name");
+
+// var capital = userinput.toUpperCase();
+// var arr=["Karachi","Lahore","Islamabad","Peshawar","Multan","Quetta"]
+
+
+var text = "My name is Abdullah Bin Aslam";
+
+for ( var i=0;i<text.length;i++){
+    if((i,i+18) === "Abdullah Bin Aslam"){
+       text = text.slice(0,i) + "Bilal Shaikh";
+       document.write(text);
+    }
+}
+
+
+
+
+
+        
